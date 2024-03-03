e983c2a93f2eeb54081c0c82e2684a0fac1f5aa0
Neetika <neetikaaryal16@gmail.com>
2024-03-03 11:55:07 +0545

    add plugins

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/index', 'AdminController@index');
+Route::get('/index', [AdminController::class, 'index']);
